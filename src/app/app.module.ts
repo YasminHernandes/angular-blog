53b3cded9fc5c67183fc1cf766a692d4566cdf4e
Yasmin Hernandes <yasminhernandesjobs@gmail.com>
2023-08-25 22:04:10 -0300

    components created with styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { TitleComponent } from './components/title/title.component';
+import { LargeCardComponent } from './components/large-card/large-card.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AngularSvgIconModule } from 'angular-svg-icon';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    TitleComponent,
+    LargeCardComponent,
+    SmallCardComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    AngularSvgIconModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
